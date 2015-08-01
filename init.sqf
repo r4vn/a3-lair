de7df63941c6e2368c8686f6715a0bc2944b0c4d
smonti <simon.schmell@gmail.com>
2015-08-02 01:07:58 +0200

    fixes #2 MenuInventory contains all kits now

--- init.sqf
+++ init.sqf
@@ -1,3 +1,11 @@
 #include "scripts\caches.sqf"
 
+[west, "SQL"] call BIS_fnc_addRespawnInventory;
+[west, "RFM"] call BIS_fnc_addRespawnInventory;
+[west, "ENG"] call BIS_fnc_addRespawnInventory;
+[west, "MED"] call BIS_fnc_addRespawnInventory;
+[west, "LAT"] call BIS_fnc_addRespawnInventory;
+[west, "MAM"] call BIS_fnc_addRespawnInventory;
+[west, "PLT"] call BIS_fnc_addRespawnInventory;
+
 call SAD_fnc_init;
---
+++ inventories.hpp
@@ -0,0 +1,220 @@
+class CfgRespawnInventory
+{
+    class SQL
+    {
+        displayName = "Officer";
+        icon = "\a3\ui_f\data\gui\cfg\Ranks\captain_gs.paa";
+
+        // Loadout definition, uses same entries as CfgVehicles classes
+        weapons[] = {
+            "arifle_MXC_F",
+            "Binocular"
+        };
+        magazines[] = {
+            "30Rnd_65x39_caseless_mag",
+            "30Rnd_65x39_caseless_mag",
+            "SmokeShell"
+        };
+        items[] = {
+            "FirstAidKit"
+        };
+        linkedItems[] = {
+            "V_Chestrig_khk",
+            "H_Watchcap_blk",
+            "optic_Aco",
+            "acc_flashlight",
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch",
+            "ItemRadio"
+        };
+        uniformClass = "U_B_CombatUniform_mcam_tshirt";
+        backpack = "B_AssaultPack_mcamo";
+    };
+    class RFM
+    {
+        displayName = "Rifleman";
+        icon = "\a3\ui_f\data\gui\cfg\Ranks\private_gs.paa";
+
+        // Loadout definition, uses same entries as CfgVehicles classes
+        weapons[] = {
+            "arifle_MXC_F",
+            "Binocular"
+        };
+        magazines[] = {
+            "30Rnd_65x39_caseless_mag",
+            "30Rnd_65x39_caseless_mag",
+            "SmokeShell"
+        };
+        items[] = {
+            "FirstAidKit"
+        };
+        linkedItems[] = {
+            "V_Chestrig_khk",
+            "H_Watchcap_blk",
+            "optic_Aco",
+            "acc_flashlight",
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch",
+            "ItemRadio"
+        };
+        uniformClass = "U_B_CombatUniform_mcam_tshirt";
+        backpack = "B_AssaultPack_mcamo";
+    };
+    class MED
+    {
+        displayName = "Medic";
+        icon = "\a3\characters_f\data\ui\icon_medic_ca.paa";
+
+        // Loadout definition, uses same entries as CfgVehicles classes
+        weapons[] = {
+            "arifle_MXC_F",
+            "Binocular"
+        };
+        magazines[] = {
+            "30Rnd_65x39_caseless_mag",
+            "30Rnd_65x39_caseless_mag",
+            "SmokeShell"
+        };
+        items[] = {
+            "FirstAidKit"
+        };
+        linkedItems[] = {
+            "V_Chestrig_khk",
+            "H_Watchcap_blk",
+            "optic_Aco",
+            "acc_flashlight",
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch",
+            "ItemRadio"
+        };
+        uniformClass = "U_B_CombatUniform_mcam_tshirt";
+        backpack = "B_AssaultPack_mcamo";
+    };
+    class LAT
+    {
+        displayName = "Rifleman Anti Tank";
+        icon = "\a3\characters_f\data\ui\icon_expl_specialist_ca.paa";
+
+        // Loadout definition, uses same entries as CfgVehicles classes
+        weapons[] = {
+            "arifle_MXC_F",
+            "Binocular"
+        };
+        magazines[] = {
+            "30Rnd_65x39_caseless_mag",
+            "30Rnd_65x39_caseless_mag",
+            "SmokeShell"
+        };
+        items[] = {
+            "FirstAidKit"
+        };
+        linkedItems[] = {
+            "V_Chestrig_khk",
+            "H_Watchcap_blk",
+            "optic_Aco",
+            "acc_flashlight",
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch",
+            "ItemRadio"
+        };
+        uniformClass = "U_B_CombatUniform_mcam_tshirt";
+        backpack = "B_AssaultPack_mcamo";
+    };
+    class ENG
+    {
+        displayName = "Engineer";
+        icon = "\a3\characters_f\data\ui\icon_repair_ca.paa";
+
+        // Loadout definition, uses same entries as CfgVehicles classes
+        weapons[] = {
+            "arifle_MXC_F",
+            "Binocular"
+        };
+        magazines[] = {
+            "30Rnd_65x39_caseless_mag",
+            "30Rnd_65x39_caseless_mag",
+            "SmokeShell"
+        };
+        items[] = {
+            "FirstAidKit"
+        };
+        linkedItems[] = {
+            "V_Chestrig_khk",
+            "H_Watchcap_blk",
+            "optic_Aco",
+            "acc_flashlight",
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch",
+            "ItemRadio"
+        };
+        uniformClass = "U_B_CombatUniform_mcam_tshirt";
+        backpack = "B_AssaultPack_mcamo";
+    };
+    class MAM
+    {
+        displayName = "Marksman";
+        icon = "\a3\ui_f\data\IGUI\Cfg\Actions\getingunner_ca.paa";
+
+        // Loadout definition, uses same entries as CfgVehicles classes
+        weapons[] = {
+            "arifle_MXC_F",
+            "Binocular"
+        };
+        magazines[] = {
+            "30Rnd_65x39_caseless_mag",
+            "30Rnd_65x39_caseless_mag",
+            "SmokeShell"
+        };
+        items[] = {
+            "FirstAidKit"
+        };
+        linkedItems[] = {
+            "V_Chestrig_khk",
+            "H_Watchcap_blk",
+            "optic_Aco",
+            "acc_flashlight",
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch",
+            "ItemRadio"
+        };
+        uniformClass = "U_B_CombatUniform_mcam_tshirt";
+        backpack = "B_AssaultPack_mcamo";
+    };
+    class PLT
+    {
+        displayName = "Pilot";
+        icon = "\a3\ui_f\data\IGUI\Cfg\Actions\getinpilot_ca.paa";
+
+        // Loadout definition, uses same entries as CfgVehicles classes
+        weapons[] = {
+            "arifle_MXC_F",
+            "Binocular"
+        };
+        magazines[] = {
+            "30Rnd_65x39_caseless_mag",
+            "30Rnd_65x39_caseless_mag",
+            "SmokeShell"
+        };
+        items[] = {
+            "FirstAidKit"
+        };
+        linkedItems[] = {
+            "V_Chestrig_khk",
+            "H_Watchcap_blk",
+            "optic_Aco",
+            "acc_flashlight",
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch",
+            "ItemRadio"
+        };
+        uniformClass = "U_B_CombatUniform_mcam_tshirt";
+        backpack = "B_AssaultPack_mcamo";
+    };
+};

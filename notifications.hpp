5ebdeca96065d84367cf2d3e999642454a17987c
smonti <simon.schmell@gmail.com>
2015-08-04 17:05:15 +0200

    added missing hpp and renamed player slots

---
+++ notifications.hpp
@@ -0,0 +1,9 @@
+class CfgNotifications {
+    class VehicleRespawn {
+        title = $STR_SAD_vehicleRespawn;
+        iconPicture = "\a3\ui_f\data\gui\cfg\Hints\vehiclegui_ca.paa";
+        description = "%1";
+        duration = 3;
+        priority = 0;
+    };
+};

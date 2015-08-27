3ee4933ef007af3d9f54da1e73a964f1ccd5b6ba
smonti <simon.schmell@gmail.com>
2015-08-27 21:07:23 +0200

    remove kunduz specific configurations

--- modules/caches/CfgModule.hpp
+++ modules/caches/CfgModule.hpp
@@ -17,24 +17,16 @@ class CfgCaches {
     // The first array entry of a building array is its class name and the
     // second is the buildingPos representing the location above terrain level
     // inside the building where the cache will be located.
-    buildings[] = {
-        {"Land_jbad_House3", 4},
-        {"Land_jbad_House5", 2},
-        {"Land_jbad_House6", 8},
-        {"Land_jbad_House7", 5},
-        {"Land_jbad_House8", 13},
-        {"Land_jbad_House_c_4", 3},
-        {"Land_jbad_House_c_5", 3},
-        {"Land_jbad_House_c_11", 7},
-        {"Land_jbad_House_3_old", 1},
-        {"Land_jbad_House_4_old", 6},
-        {"Land_jbad_House_6_old", 3},
-        {"Land_jbad_House_7_old", 5},
-        {"Land_jbad_House_8_old", 2};
-    };
+    //
+    // buildings[] = {
+    //     {"house_class_name", 0},
+    //     {"another_house_class_name", 1};
+    // };
+    buildings[] = {};
 
     class Map {
-        size = 5120;
+        // Smaller edge length of map
+        size = 6000;
     };
 
     class Marker {
@@ -48,13 +40,11 @@ class CfgCaches {
         // The first entry in a insurgent group array is the category of the
         // group and the second is the group name. The configuration path for
         // the represented group is build using these two values.
-        groups[] = {
-            {"Infantry", "IRG_ReconSentry"},
-            {"Infantry", "IRG_InfSentry"},
-            {"Infantry", "IRG_InfTeam_AT"},
-            {"Infantry", "IRG_InfTeam_MG"},
-            {"Infantry", "IRG_InfSquad"},
-            {"rhs_group_indp_ins_ural", "rhs_group_chdkz_ural_chq"};
-        };
+        //
+        // groups[] = {
+        //     {"config_category_name", "config_group_name"},
+        //     {"config_another_category_name", "config_another_group_name"};
+        // };
+        groups[] = {};
     };
 };

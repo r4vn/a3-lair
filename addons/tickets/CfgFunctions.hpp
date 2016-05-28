aecd6aaa81082ce236ce4e843047d663b884069a
r4vn <mr.monti@web.de>
2016-05-28 11:45:40 +0200

    remove client mission hud function

--- addons/tickets/CfgFunctions.hpp
+++ addons/tickets/CfgFunctions.hpp
@@ -9,11 +9,6 @@ class CfgFunctions {
                 file = "\x\lair\addons\tickets\functions\fnc_initTickets.sqf";
                 postInit = 1;
             };
-
-            class createMissionHud {
-                description = "Creates the tickets and cache count on client's hud";
-                file = "\x\lair\addons\tickets\functions\fnc_createMissionHud.sqf";
-            };
         };
     };
 };
